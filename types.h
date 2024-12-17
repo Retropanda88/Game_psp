7d4205d2fac795d2dceabeef20f1c95187f0532d
retropanda88 <andresruizperez20244@gmail.com>
2024-12-17 09:58:24 -0600

    include con tipos propios

---
+++ types.h
@@ -0,0 +1,18 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+
+//enteros sin signo
+typedef unsigned char u8,U8;
+typedef unsigned short u16, U16;
+typedef unsigned int  u32, U32;
+
+//eteros con signo
+typedef signed char s8, S8;
+typedef signed short s16, S8;
+typedef signed int s32, S32;
+
+
+
+
+#endif */end*/
