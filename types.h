e942c03608cdafa98c16189de2d62b843d5569a0
retropanda88 <andresruizperez20244@gmail.com>
2024-12-17 10:22:28 -0600

    falta de ortografia

--- types.h
+++ types.h
@@ -7,7 +7,7 @@ typedef unsigned char u8,U8;
 typedef unsigned short u16, U16;
 typedef unsigned int  u32, U32;
 
-//eteros con signo
+//enteros con signo
 typedef signed char s8, S8;
 typedef signed short s16, S8;
 typedef signed int s32, S32;
