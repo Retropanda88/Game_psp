6356c2e007e0027494b6f35452a798a42b7c13b2
retropanda88 <andresruizperez20244@gmail.com>
2024-12-19 02:43:41 -0600

    init sdl

--- src/graphic.cpp
+++ src/graphic.cpp
@@ -8,5 +8,11 @@ Graphic::Graphic(){
 }
 
 int Graphic::Init(u32 w, u32 h, const char *msg){
-
+	this->w = w;
+	this->h = h;
+	if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER)<0)
+	{
+		printf("error: %s\n",SDL_GetError());
+		return 0;
+	}
 }
